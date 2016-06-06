bcad2f5fdc866b1378821bd258e26fee26f03e1e
ing smith <ingcesarsmith@gmail.com>
2016-06-06 11:35:22 -0400

    agregado menu en agregar para importar ultimo y color a exportar

--- app/src/main/java/com/example/manuelfleivar/skynventory/Agregar.java
+++ app/src/main/java/com/example/manuelfleivar/skynventory/Agregar.java
@@ -196,7 +196,7 @@ fadquisicion.setOnFocusChangeListener(new View.OnFocusChangeListener() {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_agregar, menu);
         return true;
     }
 
--- app/src/main/java/com/example/manuelfleivar/skynventory/Exportar.java
+++ app/src/main/java/com/example/manuelfleivar/skynventory/Exportar.java
@@ -29,7 +29,7 @@ public class Exportar extends AppCompatActivity {
     public SQLiteDatabase db;
     String currentDate="";
     public DBHelper helper;
-   public Label label=new Label(0,0,"blabla" ), label1, label2, label3, label4, label5, label6, label7, label8, label9,
+   public Label label, label1, label2, label3, label4, label5, label6, label7, label8, label9,
             label10, label11, label12, label13, label14, label15, label16, label17, label18,
             label19, label20, label21, label22, label23, label24, label25, label26, label27,
             label28, label29, label30, label31, label32, label33, label34, label35, label36,
